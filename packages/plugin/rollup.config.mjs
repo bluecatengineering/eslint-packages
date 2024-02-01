e5d7f9d2f4054304befe59739a630be33ef7bf0a
JC Estibariz <jestibariz@bluecatnetworks.com>
2024-02-01 16:58:02 -0500

    chore: update dependencies

--- packages/plugin/rollup.config.mjs
+++ packages/plugin/rollup.config.mjs
@@ -1,11 +1,12 @@
 import resolve from '@rollup/plugin-node-resolve';
-import {terser} from 'rollup-plugin-terser';
+import terser from '@rollup/plugin-terser';
 
-import pkg from './package.json';
+import pkg from './package.json' assert {type: 'json'};
 
 export default {
 	input: './src/main',
 	external: Object.keys(pkg.peerDependencies),
 	output: {file: pkg.main, format: 'cjs', exports: 'default'},
 	plugins: [resolve(), terser()],
+	strictDeprecations: true,
 };
--- packages/react/rollup.config.mjs
+++ packages/react/rollup.config.mjs
@@ -1,11 +1,12 @@
 import yaml from '@rollup/plugin-yaml';
-import {terser} from 'rollup-plugin-terser';
+import terser from '@rollup/plugin-terser';
 
-import pkg from './package.json';
+import pkg from './package.json' assert {type: 'json'};
 
 export default {
 	input: './src/main',
 	external: Object.keys(pkg.dependencies),
 	output: {file: pkg.main, format: 'cjs', exports: 'default'},
 	plugins: [yaml(), terser()],
+	strictDeprecations: true,
 };
